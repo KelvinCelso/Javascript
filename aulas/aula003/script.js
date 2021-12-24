397aa23918dd7cc17cfef17cfac10092205e833a
KelvinCelso <96576338+KelvinCelso@users.noreply.github.com>
2021-12-24 15:40:13 +0200

    last updated

---
+++ aulas/aula003/script.js
@@ -0,0 +1,8 @@
+function verificar(){
+    var inicio=document.getElementById("inicio")
+    var fim=document.getElementById("fim")
+    var passos=document.getElementById("passo")
+    var proximo= Number(inicio.value)
+    if
+    
+}
